c52d0c1e1381eb10f6986aae6b483722c56219c1
Thibault Vlacich <thibault.vlacich@gmail.com>
2025-04-14 17:45:30 +0200

    test

--- src/http-example.js
+++ src/http-example.js
@@ -1,41 +1,71 @@
 import { app } from '@azure/functions';
-
-import { handleError } from '../lib/helpers.js';
+import {
+  parse as parseMultipartData,
+} from "../lib/multipart.js";
 
 app.http('http-example', {
-  methods: ['GET'],
+  methods: ['POST'],
   handler: async (request, context) => {
-    try {
-      const joke = await fetch('https://icanhazdadjoke.com/', {
-        headers: {
-          Accept: 'application/json',
-          'User-Agent': 'Azure Function App - HTTP Endpoint Demo',
-        },
-      })
-        .then(async (response) => {
-          const { joke } = await response.json();
-          return joke;
-        })
-        .catch((error) => {
-          // bubble error up to try/catch so it throws 500
-          throw error;
-        });
+      const contentType = request.headers.get('content-type')
+  const isMultipart = contentType != null && contentType.startsWith('multipart/form-data')
 
+  if (!contentType || !contentType.startsWith("multipart/form-data")) {
       return {
-        status: 200,
+        status: 400,
         body: JSON.stringify({
-          joke,
+          status: 400,
+          error: 'Invalid content type',
         }),
       };
-    } catch (error) {
-      handleError(error, context);
+  }
+  const boundary = contentType.match(/boundary=([^;]*)(;|$)/i)?.[1];
+  if (!boundary) {
       return {
-        status: 500,
+        status: 400,
         body: JSON.stringify({
-          status: 500,
-          error,
+          status: 400,
+          error: 'Boundary not found in content type',
         }),
       };
+  }
+
+  const formData = await request.formData();
+  const formDataFile = formData.get("file");
+
+  if(typeof formDataFile === "string") {
+    return {
+      status: 400,
+      body: JSON.stringify({
+        status: 400,
+        error: 'File is not a Blob',
+      }),
+    };
+  }
+
+
+  //console.log(await formDataFile.text())
+  console.log(formDataFile.bytes.length)
+
+  const bodyText = await request.text();
+  //console.log(bodyText)
+  const h3FormData = parseMultipartData(Buffer.from(bodyText), boundary);
+  const file = h3FormData.find((partData) => partData.name === "file");
+
+    if (!file) {
+      return {
+        status: 400,
+        body: JSON.stringify({
+          status: 400,
+          error: 'Missing file in form data',
+        }),
+      };
+    }
+
+    return {
+      status: 200,
+      body: JSON.stringify({
+        fileSize: file.data.byteLength
+      })
     }
   },
 });
